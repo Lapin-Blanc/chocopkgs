78f2a7f992465ad680c566a0c405d71ee3889db4
Lapin-Blanc <fabien.toune@gmail.com>
2020-10-29 10:39:13 +0100

    improved presentation

--- packages/admin.py
+++ packages/admin.py
@@ -3,6 +3,8 @@ from .models import ChocoPackage
 
 
 class ChocoPackageAdmin(admin.ModelAdmin):
-    list_display = ['software_name', 'package_name', 'software_description', 'selected_by_default']
+    list_display = ['software_name', 'package_name', 'software_description',
+                    'selected_by_default']
+
 
 admin.site.register(ChocoPackage, ChocoPackageAdmin)
